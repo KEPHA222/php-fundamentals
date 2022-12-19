49595cc6709ee9bfed051508edf59de1c74a1b2d
kepha222 <kephamotari1@gmail.com>
2022-12-20 01:21:10 +0300

    third commit

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -2,15 +2,13 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'kepha');
-define('DB_PASS', '1234');
-define('DB_NAME', 'php_dev');
+define('DB_PASS', 'password');
+define('DB_NAME', 'phpfundamentals');
 
 //Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 //Check Connection
 if($conn->connect_error) {
-    die('Connection Failed ' . $conn->coonect_error);
+    die('Connection Failed ' . $conn->connect_error);
 }
-
-echo 'CONNECTED!';
